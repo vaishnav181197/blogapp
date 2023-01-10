b779d91a7b08049532a9a532b55f7917efed9dc3
ysh <vvcvaishnav@gmail.com>
2023-01-10 10:19:23 +0530

    url changes

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v8nywfr-q)-^&e+t(&5_+70*b2o@ur5le4sh2dk$wuv-^7s!oj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://vaishnav181197.github.io/"]
 
 
 # Application definition
